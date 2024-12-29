85ac20688bf1e542380b6f00f32f5f1ad58d03d8
matthewlu070111 <matthewlu070111@outlook.com>
2024-12-29 21:15:50 +0800

    Update crafting table, updating translation, bring a new thing: Dough Scrap!

--- src/main/java/com/imxiaoanag/xiaoanfc/item/FarmingCoreItemGroupRegister.java
+++ src/main/java/com/imxiaoanag/xiaoanfc/item/FarmingCoreItemGroupRegister.java
@@ -5,19 +5,19 @@ import com.imxiaoanag.xiaoanfc.block.FarmingCoreBlockRegister;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class FarmingCoreItemGroupRegister {
-    public static final ItemGroup XIAOANFC_GROUP = FabricItemGroup.builder()
-            .icon(() -> new ItemStack(Items.WHEAT))
-            .displayName(Text.translatable("itemGroup.xiaoanfc.xiaoanfc_group"))
+    public static final ItemGroup XIAOANFC_CORE = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(FarmingCoreBlockRegister.GRINDER))
+            .displayName(Text.translatable("itemGroup.xiaoanfc.xiaoanfc_core"))
             .entries((context, entries) -> {
-                entries.add(FarmingCoreItemRegister.DOUGH);
                 entries.add(FarmingCoreItemRegister.FLOUR);
+                entries.add(FarmingCoreItemRegister.DOUGH);
+                entries.add(FarmingCoreItemRegister.DOUGH_SCRAPS);
                 entries.add(FarmingCoreItemRegister.STIR_STICK);
                 entries.add(FarmingCoreItemRegister.MIXING_BOWL);
 
@@ -26,7 +26,7 @@ public class FarmingCoreItemGroupRegister {
             .build();
 
     public static void initialize(){
-        Registry.register(Registries.ITEM_GROUP, Identifier.of(XiaoansFarmingCore.MOD_ID, "xiaoanfc_group"), XIAOANFC_GROUP);
+        Registry.register(Registries.ITEM_GROUP, Identifier.of(XiaoansFarmingCore.MOD_ID, "xiaoanfc_materials"), XIAOANFC_CORE);
 
         XiaoansFarmingCore.LOGGER.info("Item group registration succeed!");
     }
